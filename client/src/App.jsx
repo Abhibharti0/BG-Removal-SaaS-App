5be66906b5e154aeedd5e6b6e0198761cdd19340
Abhishek Bharti <abhibharti457@gmail.com>
2025-12-20 22:00:09 +0530

    setup frontend

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-900">
+      <h1 className="text-4xl font-bold text-sky-400">
+        initialise project with Vite + React + TailwindCSS
+      </h1>
+    </div>
+  )
+}
+
+export default App
